b97f846d43b0e807f9455eab1d1ccaf74f6d1cd2
Nicolás Pennesi <nicolaspennesi@gmail.com>
2015-02-16 05:06:40 -0300

    Funcionando opciones para definir pin y comando

--- xbee/main.c
+++ xbee/main.c
@@ -31,7 +31,11 @@
 
 sem_t semaforo; //Declaramos la vaiable en la que estaá el semaforo
 
-void analizarespuesta(xbee_err res, unsigned char resval){
+//------------------
+//-- FUNCIONES
+//------------------
+
+void analizaRespuestaCoordinador(xbee_err res, unsigned char resval){
 	if (res == XBEE_ETX) {
 		if (resval == 4) {
 			fprintf(stderr, "ERROR: NODO REMOTO NO ENCONTRADO\n");
@@ -44,14 +48,14 @@ void analizarespuesta(xbee_err res, unsigned char resval){
 }
 
 //Función callback, se ejecuta cuando el xbee coordinador recibe respuesta de un nodo.
-void cambiar(struct xbee *xbee, struct xbee_con *con, struct xbee_pkt **pkt, void **data) {
+void analizarRespuestaNodoRemoto(struct xbee *xbee, struct xbee_con *con, struct xbee_pkt **pkt, void **data) {
 
 	printf("CALLBACK 2\n");
 
 	//Muestra respuesta del nodo (0 es OK)
 	switch (*(*pkt)->data){
 		case 0: //Si el pin está en bajo, lo pone en alto
-			printf("NODO REMOTO DICE: OK");
+			printf("NODO REMOTO DICE: OK\n");
 			break;
 		default:
 			fprintf(stderr, "NODO REMOTO DICE: ERROR (0x%02X)\n", *(*pkt)->data);
@@ -64,16 +68,17 @@ void cambiar(struct xbee *xbee, struct xbee_con *con, struct xbee_pkt **pkt, voi
 }
 
 //Función callback, se ejecuta cuando el xbee coordinador recibe respuesta de un nodo.
-void evaluar(struct xbee *xbee, struct xbee_con *con, struct xbee_pkt **pkt, void **data) {
+void evaluaryCambiar(struct xbee *xbee, struct xbee_con *con, struct xbee_pkt **pkt, void **data) {
 
 	unsigned char txErr;
 	xbee_err err;
+	char *pinConsultado = (*pkt)->atCommand; //En (*pkt)->atCommand se encuentra el pin por el cual se consultó
 
 	printf("CALLBACK 1\n");
 	printf("EVALUANDO..\n");
 
 	//Setea la función callback
-	if ((err = xbee_conCallbackSet(con, cambiar, NULL)) != XBEE_ENONE) {
+	if ((err = xbee_conCallbackSet(con, analizarRespuestaNodoRemoto, NULL)) != XBEE_ENONE) {
 		xbee_log(xbee, -1, "xbee_conCallbackSet() returned: %d", err);
 	}
 
@@ -81,26 +86,25 @@ void evaluar(struct xbee *xbee, struct xbee_con *con, struct xbee_pkt **pkt, voi
 	switch (*(*pkt)->data){
 		case BAJO: //Si el pin está en bajo, lo pone en alto
 			printf("ESTADO: APAGADO\n");
-			err = xbee_conTx(con, &txErr, "%s%c", PIN2, ALTO);
-			if (err != XBEE_ENONE) {
-				analizarespuesta(err, txErr);
-			} else{
-				printf("ENVIANDO COMANDO ENCENDER...\n");
-			}
+			err = xbee_conTx(con, &txErr, "%s%c", pinConsultado, ALTO);
+			printf("ENVIANDO COMANDO ENCENDER...\n");
 			break;
 		case ALTO: //Si el pin está en alto, lo pone en bajo
 			printf("ESTADO: ENCENDIDO\n");
-			err = xbee_conTx(con, &txErr, "%s%c", PIN2, BAJO);
-			if (err != XBEE_ENONE) {
-				analizarespuesta(err, txErr);
-			} else{
-				printf("ENVIANDO COMANDO APAGAR...\n");
-			}
+			err = xbee_conTx(con, &txErr, "%s%c", pinConsultado, BAJO);
+			printf("ENVIANDO COMANDO APAGAR...\n");
 			break;
 		default:
 			fprintf(stderr, "ERROR AL INTERPRETAR RESPUESTA DEL NODO REMOTO (0x%02X)\n", *(*pkt)->data);
 			return;
 			break;
+	}
+
+	//Evalúa si hubo algún error al enviar el comando
+	if (err != XBEE_ENONE) {
+		analizaRespuestaCoordinador(err, txErr);
+	} else{
+		printf("COMANDO ENVIADO\n");
 	}
 
 }
@@ -111,8 +115,9 @@ int main(int argc, char *const argv[]){
 
 	char *archconfig = "default.cfg"; //Puntero al nombre del archivo de configuración. Por defecto es default.cfg
 
-	char *remoteaddr = "0013A2004086A3FF";
-	char *comando;
+	char *remoteaddr = "0013A2004086A3FF"; //Setea la dirección por del nodo remoto
+	char comando = 'c'; //El comando por defecto es cambiar el estado del pin
+	char *pin = PIN1; //El pin por defecto es el pin 1
 	char *puertoserie;
 	char *baudiosstring;
 	int baudios;
@@ -133,21 +138,49 @@ int main(int argc, char *const argv[]){
 	xbee_err err;
 
 	//Se analizan los argumentos
-	while ((opcion = getopt (argc, argv, "f:r:c:")) != -1){
+	while ((opcion = getopt (argc, argv, "f:r:p:c:")) != -1){
 		switch (opcion){
-			case 'f':
+			case 'f': //archivo de configuracion
 				archconfig = optarg;
 				break;
-			case 'r':
+			case 'r': //serial del nodo remoto
 				remoteaddr = optarg;
 				break;
-			case 'c':
-				comando = optarg;
+			case 'p': //pin del nodo remoto
+				//Evaluamos si el valor pasado por argumento para el pin es 1, 2, 3 o 4
+				switch (atoi(optarg)){
+					case 1:
+						pin = PIN1;
+						break;
+					case 2:
+						pin = PIN2;
+						break;
+					case 3:
+						pin = PIN3;
+						break;
+					case 4:
+						pin = PIN4;
+						break;
+					default:
+						fprintf (stderr, "Pin Incorrecto (Solo se admite 1, 2, 3 o 4).\n");
+						return 1;
+				}
+				break;
+			case 'c': //comando nodo remoto (c: cambiar, e: encender, a= apagar)
+				//Para esta opcion solo se evalúa el primer caracter del argumento
+				if(*optarg == 'c' || *optarg == 'e' || *optarg == 'a'){
+					comando = *optarg; //Guarda el primer caracter del argumento en la variable comando
+				} else {
+					fprintf (stderr, "El argumento de la opción 'c' sólo puede ser c: cambiar, e: encender, a: apagar.\n");
+					return 1;
+				}
 				break;
 			case '?':
 				if (optopt == 'f') {
 					fprintf (stderr, "La opción -%c requiere un argumento.\n", optopt);
 				} else if (optopt == 'r') {
+					fprintf (stderr, "La opción -%c requiere un argumento.\n", optopt);
+				} else if (optopt == 'p') {
 					fprintf (stderr, "La opción -%c requiere un argumento.\n", optopt);
 				} else if (optopt == 'c') {
 					fprintf (stderr, "La opción -%c requiere un argumento.\n", optopt);
@@ -163,7 +196,7 @@ int main(int argc, char *const argv[]){
 
 	//Leemos el archivo de configuracion y guardamos sus datos en las variables globales correspondientes
 	if ((descarch = open(archconfig,O_RDONLY,0)) < 0){
-		fprintf(stderr, "Error al abrir el archivo de configuración especificado");
+		fprintf(stderr, "Error al abrir el archivo de configuración especificado\n");
 		return -1;
 	}
 	leido = read (descarch, buffer, sizeof buffer);
@@ -195,16 +228,44 @@ int main(int argc, char *const argv[]){
 		return err;
 	}
 
-	//Setea la función callback
-	if ((err = xbee_conCallbackSet(con, evaluar, NULL)) != XBEE_ENONE) {
-		xbee_log(xbee, -1, "xbee_conCallbackSet() returned: %d", err);
-		return err;
+	//Evalúa el comando
+	switch (comando){
+		case 'c': //En caso de que sea cambiar (de estado)
+			//Setea la función callback
+			if ((err = xbee_conCallbackSet(con, evaluaryCambiar, NULL)) != XBEE_ENONE) {
+				xbee_log(xbee, -1, "xbee_conCallbackSet() returned: %d", err);
+				return err;
+			}
+			//Envía consulta al nodo remoto (pregunta por estado de del pin)
+			err = xbee_conTx(con, &txErr, pin);
+			printf("ENVIANDO COMANDO CAMBIAR ESTADO...\n");
+			break;
+		case 'e': //En caso de que sea encender
+			//Setea la función callback
+			if ((err = xbee_conCallbackSet(con, analizarRespuestaNodoRemoto, NULL)) != XBEE_ENONE) {
+				xbee_log(xbee, -1, "xbee_conCallbackSet() returned: %d", err);
+				return err;
+			}
+			//Envía consulta al nodo remoto (pregunta por estado de del pin)
+			err = xbee_conTx(con, &txErr, "%s%c", pin, ALTO);
+			printf("ENVIANDO COMANDO ENCENDER...\n");
+			break;
+
+		case 'a': //En caso de que sea apagar
+			//Setea la función callback
+			if ((err = xbee_conCallbackSet(con, analizarRespuestaNodoRemoto, NULL)) != XBEE_ENONE) {
+				xbee_log(xbee, -1, "xbee_conCallbackSet() returned: %d", err);
+				return err;
+			}
+			//Envía consulta al nodo remoto (pregunta por estado de del pin)
+			err = xbee_conTx(con, &txErr, "%s%c", pin, BAJO);
+			printf("ENVIANDO COMANDO APAGAR...\n");
+			break;
 	}
 
-	//Envía consulta al nodo remoto (pregunta por estado de DEL PIN2)
-	err = xbee_conTx(con, &txErr, PIN2);
+	//Evalúa si hubo algún error al enviar el comando
 	if (err != XBEE_ENONE) {
-		analizarespuesta(err, txErr);
+		analizaRespuestaCoordinador(err, txErr);
 	} else{
 		printf("COMANDO ENVIADO\n");
 		//Queda esperando respuesta del nodo así tiene tiempo de entrar en la funcion callback
