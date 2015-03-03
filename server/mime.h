e63b0b0c959e96a31709a0b100e6ddf91f66147b
Nicolás Pennesi <nicolaspennesi@gmail.com>
2015-03-03 01:37:16 -0300

    Limpio Raiz

--- server/mime.h
+++
@@ -1,4 +0,0 @@
-#ifndef _MIME_H_
-#define _MIME_H_
-char *mime_gettype(char *file);
-#endif
--- xbee/utilidades.h
+++
@@ -1,6 +0,0 @@
-#ifndef _UTILIDADES_H_
-	#define _UTILIDADES_H_
-
-	int stringahex ( char*, char*);
-
-#endif
